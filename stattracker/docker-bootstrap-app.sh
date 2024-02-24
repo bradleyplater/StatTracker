34e53fcb71984a1fc57c31bc0e48e734f53ad9f2
bradleyplater <bradley.plater1@gmail.com>
2024-02-24 17:53:33 +0000

    Commented out prisma migrate deploy

--- stattracker/docker-bootstrap-app.sh
+++ stattracker/docker-bootstrap-app.sh
@@ -2,6 +2,6 @@
 # ENVIRONEMTN from docker-compose.yaml doesn't get through to subprocesses
 # Need to explicit pass DATABASE_URL here, otherwise migration doesn't work
 # Run migrations
-npx prisma migrate deploy
+# npx prisma migrate deploy
 # start app
 npm run start

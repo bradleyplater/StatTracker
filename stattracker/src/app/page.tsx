26ca36caa7f5f1698fdf5bc19ace1c2916d3aeac
bradleyplater <bradley.plater1@gmail.com>
2024-03-05 11:53:11 +0000

    Updated root page to redirect to profile if you are logged in

--- stattracker/src/app/page.tsx
+++ stattracker/src/app/page.tsx
@@ -1,4 +1,5 @@
 import { getSession } from '@auth0/nextjs-auth0';
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
     const session = await getSession();
@@ -6,7 +7,7 @@ export default async function Home() {
     return (
         <>
             {session?.user ? (
-                <h1 className="text-slate-900">You are logged in</h1>
+                redirect('/Profile')
             ) : (
                 <h1 className="text-slate-900">You are logged out</h1>
             )}

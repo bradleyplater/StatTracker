1716dbd866e0d848c2d111178afd8dd66c8a066c
bradleyplater <bradley.plater1@gmail.com>
2024-03-10 21:31:14 +0000

    removed base path

--- stattracker/next.config.js
+++ stattracker/next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com'],
     },
-    basePath: process.env.BASE_URL,
     experimental: {
         serverActions: {
             allowedOrigins: ['stattracker.co.uk'],
